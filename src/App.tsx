8a450d7429ccf800db7dfe4e9fdc0c182c054780
martymcquaid <martymcquaid@users.noreply.github.com>
2025-12-01 17:02:41 +0000

    AI edit: /root/ChitterChat/team-call-connect/TwilioAPIService/chipify_projects/67fcacf6-d880-4ccf-8a59-3d8bd92bb6e7/src/App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,19 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Navigation from './components/Navigation'
 import Home from './pages/Home'
+import Services from './pages/Services'
+import About from './pages/About'
+import Contact from './pages/Contact'
 
 function App() {
   return (
     <BrowserRouter>
+      <Navigation />
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/services" element={<Services />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
       </Routes>
     </BrowserRouter>
   )
